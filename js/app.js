3f33d4e48ae85034a16d7a37fb7ac34ef7f93af1
lespicker <lespicker@gmail.com>
2018-04-17 15:14:21 -0400

    email subscribe

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$( function(){
+    $("#news").submit(function (event) {
+      $("#subscribe").prop("disabled", true);
+      $("#spinner").addClass("dib");
+      console.log(data = $(this).serializeArray());
+      event.preventDefault();
+      p = { "email_address": data[0].value,
+        "status": "subscribed",
+        "merge_fields": {}
+      };
+      fetch("https://json-api-wuyywqnnuj.now.sh", {
+        headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+        },
+        "method": "POST",
+        body: JSON.stringify(p)
+      }).then(response => {
+        $("#spinner").removeClass("dib");
+        $('#news').trigger("reset");
+        $("#subscribe").prop("disabled", false);
+      });
+    });
+  });
